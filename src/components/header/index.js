e4cc93bb6c10430f0256ba7a42d88a0c13eec9e0
Amel AZZI <ea_azzi@esi.dz>
2018-11-17 01:00:18 +0100

    add social media icons & links in the header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import Link from 'gatsby-link';
+
 import Logo from './logo';
 import './header.scss';
-const Header = ({ siteTitle }) => (
+
+var SocialMedia =[
+    {icon: require("../../icons/twitter-icon.png"), link: "https://twitter.com"},
+    {icon: require("../../icons/linkedin-icon.png"), link: "https://www.linkedin.com/"},
+    {icon: require("../../icons/github-icon.png"), link: "https://github.com"}
+];
+
+const SocialMediaList = SocialMedia.map((social) =>
+    <span className="overlay">
+		<a key={social.icon} href={social.link}>
+			<img className="social-icons" key={social.icon} src={social.icon}/> 
+		</a>
+	</span>
+);
+
+
+const Header = ({ socialMedia }) => (
 	<header>
 		<div className="header-container">
 			<Logo title="Andelus" />
+		</div>
+		<div className="social-media">
+			{SocialMediaList}
 		</div>
 	</header>
 );
